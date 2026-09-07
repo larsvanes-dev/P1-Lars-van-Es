d765e739f3b66838abe44047bcac747cabe829ad
larsvanes-dev <larsvanes.zakelijk@gmail.com>
2026-09-07 10:53:03 +0200

    Update sketch.js

--- Week 1/sketch.js
+++ Week 1/sketch.js
@@ -4,4 +4,20 @@ function setup() {
 
 function draw() {
   background(220);
+
+  // Dit is de tekst
+  fill(0,0,0);
+  text("Lars van Es", 5, 10);
+
+  // Dit is de vlag
+  noStroke();
+  fill(255,255,255);
+  rect(5, 15, 120, 90);
+  fill(255,0,0);
+  rect(5, 15, 120, 30);
+  fill(0,0,255);
+  rect(5, 75, 120, 30);
+
+  // Dit is het schaakbord
+  
 }
