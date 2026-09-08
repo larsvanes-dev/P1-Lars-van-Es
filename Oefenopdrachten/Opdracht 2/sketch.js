e700ad570cad57614abdc8a6f8ce09a0cba42ff0
larsvanes-dev <larsvanes.zakelijk@gmail.com>
2026-09-08 13:30:24 +0200

    Oefenopdracht 2 gemaakt

--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -1,7 +1,32 @@
+let x = 100;
+let greeting = "Hello world!"
+let a = 20;
+let b = 10;
+let optellen;
+let aftrekken;
+let vermenigvuldigen;
+let delen;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  // Variabelen veranderen
+  optellen = a + b;
+  aftrekken = a - b;
+  vermenigvuldigen = a * b;
+  delen = a / b;
+
+  // Alle teksten
+  text(x, 20, 20);
+  text(greeting, 20, 60);
+
+  // Tekst + waarde van variabele
+  text("Optellen: " + optellen, 20, 80);
+  text("Aftrekken: " + aftrekken, 20, 100);
+  text("Vermenigvuldigen: " + vermenigvuldigen, 20, 120);
+  text("Delen: " + delen, 20, 140);
+
 }
---
+++ Test/Afbeelding/sketch.js
@@ -0,0 +1,16 @@
+let israel;
+let israelFrame = 0;
+
+function preload() {
+  israel = loadImage('assets/israel-inform-israel.gif');
+}
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(220);
+  israelFrame = abs(mouseX / 10);
+  image(israel, mouseX - 100, mouseY - 100, 200, 200);
+  israel.setFrame(israelFrame);
+}
